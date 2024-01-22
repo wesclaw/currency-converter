1a54e2593272512869a79f07c1b0bd6fbe27c92c
wesclaw <108541397+wesclaw@users.noreply.github.com>
2024-01-22 13:22:48 +0100

    update

--- js/index.js
+++ js/index.js
@@ -10,20 +10,8 @@ const countrySelectTo = document.getElementById('country-select-from')
 // arrows button
 const arrowBtn = document.querySelector('.arrow-btn')
 
-
 function displayCurrency(){
-  const getInputAmountValue = amountInput.value
-  const getCountryAmountValue = countrySelectFrom.value
-
-  const getConvertedFromInputValue = convertedToInput.value;
-  const getCountryConvertedToValue = countrySelectTo.value
-
-  console.log(getInputAmountValue)
-
-  fetch(`https://v6.exchangerate-api.com/v6/ca34e3167596feec8cde346e/latest/${getCountryAmountValue}`).then((res)=>res.json()).then((data)=>{
-    const rate = data.conversion_rates
-  })
-
+ 
 }
 
 
